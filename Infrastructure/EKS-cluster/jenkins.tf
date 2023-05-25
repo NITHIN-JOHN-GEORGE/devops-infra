f4a47e1d08f7592aac70f5732e8cb8445d00a312
nithinjohn <nithinjohn97@gmail.com>
2023-05-25 23:38:05 +0530

    EKS

--- Infrastructure/EKS-cluster/jenkins.tf
+++ Infrastructure/EKS-cluster/jenkins.tf
@@ -1,13 +1,25 @@
 data "template_file" "jenkins" {
   template = <<EOF
-jenkinsUser: "${var.jenkinsUsername}"
-jenkinsPassword: "${var.jenkinsPassword}"
-plugins: [blueocean,ws-cleanup,job-dsl,role-strategy,parameterized-trigger,nodejs,generic-webhook-trigger,credentials,credentials-binding]
+
+controller:
+  adminUser: "${var.jenkinsUsername}"
+  adminPassword: "${var.jenkinsPassword}"
+
+   installPlugins:
+     - kubernetes:3900.va_dce992317b_4
+     - workflow-aggregator:596.v8c21c963d92d
+     - git:5.0.0
+     - configuration-as-code:1625.v27444588cc3d
+     - blueocean
+     - ws-cleanup
+     - nodejs
+
 
 persistence:
-   enabled: true
-   storageClass: "ebs-sc"
-   size: 8Gi
+  enabled: true
+  storageClass: ebs-sc
+  accessMode: "ReadWriteOnce"
+  size: "8Gi"
 
    EOF
 }
@@ -18,7 +30,7 @@ resource "helm_release" "jenkins" {
   namespace        = "jenkins"
   create_namespace = true
   name                = "jenkins"
-  repository          = "oci://registry-1.docker.io/bitnamicharts/jenkins"
+  repository          = "https://charts.jenkins.io"
   chart               = "jenkins"
   values = [data.template_file.jenkins.rendered]
 
