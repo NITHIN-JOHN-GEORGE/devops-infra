0d5a0ab28fbd7aaa8050d3fe54e4d29bba90de24
nithinjohn <nithinjohn97@gmail.com>
2023-05-26 13:58:14 +0530

    EKS

--- Infrastructure/EKS-cluster/argo-cd.tf
+++ Infrastructure/EKS-cluster/argo-cd.tf
@@ -4,38 +4,13 @@ dex:
   enabled: false 
 redis-ha:
   enabled: false
-controller:
-  enableStatefulSet: true
 server:
   extraArgs:
     - --insecure
-  image:
-    repository: argoproj/argocd
-    tag: latest
-    pullPolicy: IfNotPresent
-  replicas: 2
-  resources:
   containerPorts:
       server:  8080
-  ingress:
-    enabled: true
-    ingressClassName: "alb"
-    annotations:
-      alb.ingress.kubernetes.io/group.name: argocd
-      alb.ingress.kubernetes.io/scheme: internet-facing
-      alb.ingress.kubernetes.io/backend-protocol: HTTP
-      alb.ingress.kubernetes.io/listen-ports: '[{"HTTP":80}]'
-      alb.ingress.kubernetes.io/target-type: ip
-    hosts:
-    - "argocd.example.io"
-  ingressGrpc:
-    enabled: true
-    isAWSALB: true
-    awsALB:
-      serviceType: NodePort
-      backendProtocolVersion: HTTP2
   service:
-    type: NodePort
+    type: LoadBalancer
   serviceAccount:
     create: true
     name: argocd-server
